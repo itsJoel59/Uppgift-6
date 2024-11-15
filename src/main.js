4c934f882d84fa393413ecf6371288ea8402e039
itsJoel59 <joel.j.jansson@gmail.com>
2024-11-15 07:39:10 +0100

    Tried something called coverage

---
+++ src/main.js
@@ -0,0 +1,29 @@
+const inputField = document.querySelector("#inputField");
+const inputBtn = document.querySelector("#inputBtn");
+
+inputBtn.addEventListener(
+    "click",
+    function(event){
+        let input = inputField.value;
+        const resultOutput = document.querySelector("#result");
+        const error = document.querySelector("#error");
+        if (input == "" || input == null){
+
+            error.innerHTML = "<span style='color: red;'>" +
+            "Write a word plz</span>";
+        }
+        else{
+            error.innerHTML == "";
+            let result = toCapital(input);
+            resultOutput.innerHTML = result;
+            console.log(result);
+            inputField.value = null;
+        }
+    },
+    false
+);
+
+export function toCapital(string){
+
+    return string.toUpperCase();
+}
